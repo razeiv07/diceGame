65840d7a56b5ee01d8c22aa0bc52bfad81f1a9fe
razeiv07 <rajeevxrma@gmail.com>
2024-07-04 22:28:31 -0400

    Second Commit- Minor change of changing dices when onload

--- Dice Game/ViewController.swift
+++ Dice Game/ViewController.swift
@@ -8,10 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        diceImageView1.image = UIImage(named: "DiceOne")
+        
+        // Change the image to dice six after the view loads
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.diceImageView1.image = UIImage(named: "DiceThree")
+        }
+        
+        
+        diceImageView2.image = UIImage(named: "DiceOne")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.diceImageView2.image = UIImage(named: "DiceTwo")
+        }
     }
 
 
