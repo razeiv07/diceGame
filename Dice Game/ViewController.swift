4ad44a75e84e4a527cd5cd04d6519f1762aaa2c4
razeiv07 <rajeevxrma@gmail.com>
2024-07-04 21:35:54 -0400

    Initial Commit

---
+++ Dice Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice Game
+//
+//  Created by Rajeev Sharma on 2024-07-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
