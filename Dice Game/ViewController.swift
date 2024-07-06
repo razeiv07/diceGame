cf33447cfbc7e2fa1d1564677d7dc882c13dc5bc
razeiv07 <rajeevxrma@gmail.com>
2024-07-06 12:46:42 -0400

    random dice will be generated

--- Dice Game/ViewController.swift
+++ Dice Game/ViewController.swift
@@ -11,30 +11,17 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-//        diceImageView1.image = UIImage(named: "DiceFour")
-//        diceImageView2.image = UIImage(named: "DiceSix")
-    }
 
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
+        var diceArray = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
+        
 //        diceImageView1.image = UIImage(named: "DiceTwo")
 //        diceImageView2.image = UIImage(named: "DiceOne")
-        diceImageView1.image = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")] [leftDiceNumber]
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
         
-        leftDiceNumber = leftDiceNumber + 1
-        
-        diceImageView2.image = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")] [rightDiceNumber]
-        
-        rightDiceNumber = rightDiceNumber - 1
     }
 }
 
